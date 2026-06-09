b1ec39f4b80de19fe1412ee856cbcebd1a64fe61
alyssa375 <alyssa@ayohfoods.com>
2026-06-09 16:02:47 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   env: {
     NEXT_PUBLIC_SUPABASE_URL: 'https://ejfiffogznxqrppvirxf.supabase.co',
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVqZmlmZm9nem54cXJwcHZpcnhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwMjA5NjgsImV4cCI6MjA5NjU5Njk2OH0.JkAkI-3TrlBSiB5f5foHsq9IAqWDE-c8Rnwd4KpB4SE',
   },
   images: {
     remotePatterns: [
